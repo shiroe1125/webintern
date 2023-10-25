a97159e94691ef09c1bb335eb3aea400773b6683
shiroe1125 <leanhquang6@gmail.com>
2023-10-25 19:49:48 +0700

    home

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('page.login');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = [
+            'email'=>$request->input('email'),
+            'password'=>$request->input('password')
+        ];
+
+        if(Auth::attempt($data)){
+            return redirect()->intended('/');
+        }
+        else {
+            echo 1;
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/page/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('page.layout.head')
+
+<body>
+	<!--/header-->
+    @include('page.layout.header')
+	
+	<!--/slider-->
+	@include('page.layout.slider')
+	
+	@include('page.layout.list_product')
+	<!--/Footer-->
+    @include('page.layout.footer')
+	@include('page.layout.script')
+</body>
+</html>
